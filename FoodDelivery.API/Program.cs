3d16c3906b2e13cbc8f3c24e941e8f822efd3d68
Dineshhank <babud2000@gmail.com>
2026-03-10 23:06:12 +0530

    Razorpay and Program.cs file updated

--- FoodDelivery.API/Program.cs
+++ FoodDelivery.API/Program.cs
@@ -86,7 +86,7 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
