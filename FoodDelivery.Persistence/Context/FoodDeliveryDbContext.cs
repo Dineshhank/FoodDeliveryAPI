966ff572d5e805def6fca5fe9ab413df65dfc370
Dineshhank <babud2000@gmail.com>
2026-03-20 12:28:18 +0530

    updated connectionstrings local to supabase

--- FoodDelivery.Persistence/Context/FoodDeliveryDbContext.cs
+++ FoodDelivery.Persistence/Context/FoodDeliveryDbContext.cs
@@ -52,9 +52,9 @@ public partial class FoodDeliveryDbContext : DbContext
 
     public virtual DbSet<Userrole> Userroles { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=fooddelivery_db;Username=postgres;Password=postgres123");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=fooddelivery_db;Username=postgres;Password=postgres123");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
